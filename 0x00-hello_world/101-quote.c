17c6533e10a79b32f5605dc343f32b89f348f733
anassouakkouch <ouakkouch.anass@gmail.com>
2023-03-16 17:22:05 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - A c program that prints a line to the standard error
- * Return: Always 1 (Success)
+ *  main - A c program that prints a line to the standard e
+rror
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\R, stdout);
+	fput("and that piece of art is useful\" - Dora kor
+par, 2015-10-19\n", stdout);
 	return (1);
 }
