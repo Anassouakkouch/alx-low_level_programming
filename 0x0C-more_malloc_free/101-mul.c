c47df8186ea7e448ef75ef23d6ab4616ca5b089a
anassouakkouch <ouakkouch.anass@gmail.com>
2023-04-13 17:04:10 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,109 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: poiter represents memory block to fill
- * @b: characters to fill
- * @n: number of bytes to fill
- * Return: ^pointer to filled memory area
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
-char *_memset(char *s, char b, unsigned int n)
+int is_digit(char *s)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (i < n)
+	while (s[i])
 	{
-		s[i] = b;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 		i++;
 	}
-	return (s);
+	return (1);
 }
 
 /**
- * _calloc - allocates memory
- * @nmemb: size of array
- * @size: of elements
- * Return: pointer to new allocated memory
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+int _strlen(char *s)
 {
-	char *p;
+	int i = 0;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	_memset(p, 0, nmemb * size);
-	return (p);
-}
-
-/**
- * multiply - initialize array whith 0 byte
- * @s1: string
- * @s2: string
- */
-
-void multiply(char *s1, char *s2)
-{
-	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
-	char *ptr;
-	void *temp;
-
-	l1 = _length(s1);
-	l2 = _length(s2);
-	tmp = l2;
-	total_l = l1 + l2;
-	ptr = _calloc(sizeof(int), total_l);
-	temp = ptr;
-
-	for (l1-- ; l1 >= 0; l1--)
+	while (s[i] != '\0')
 	{
-		f_digit = s1[l1] - '0';
-		res = 0;
-		l2 = tmp;
-		for (l2-- ; l2 >= 0 ; l2--)
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			s_digit = s2[l2] - '0';
-			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
-			ptr[l1 + l2 + 1] = res % 10;
-			res /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (res)
-			ptr[l1 + l2 + 1] = res % 10;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	while (*ptr == 0)
+	for (i = 0; i < len - 1; i++)
 	{
-		ptr++;
-		total_l--;
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	for (i = 0; i < total_l; i++)
-		printf("%i", ptr[i]);
-	printf("\n");
-	free(temp);
-}
-
-/**
- * main - entry point
- * @argc: num of arg
- * @argv: arg of array
- * Return: 0 or 98
- */
-
-int main(int argc, char **argv)
-{
-	char *n1 = argv[1], *n2 = argv[2];
-
-	if (argc != 3 || check_number(n1) || check_number(n2))
-		error_exit();
-	if (*n1 == '0' || *n2 == '0')
-	{
+	if (!a)
 		_putchar('0');
-		_putchar('\n');
-	}
-	else
-		multiply(n1, n2);
+	_putchar('\n');
+	free(result);
 	return (0);
 }
