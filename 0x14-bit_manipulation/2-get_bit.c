41daa801d8411527203e459adb83f42a70c167e4
anass ouakkouch <ouakkouch.anass@gmail.com>
2023-05-04 07:36:35 -0700

    get mani

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
-  @index:index unsigned int
- * Return: bit at given index else -1 if error accured
+ * get_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int power;
+	unsigned long int i;
 
 	if (index > 64)
 		return (-1);
 
-	power = n >> index;
-
-	return (power & 1);
+	m = 1 << index;
+	return ((n & i) > 0);
 }
